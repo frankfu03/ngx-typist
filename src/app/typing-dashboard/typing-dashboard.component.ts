609aa9b4a1b8ab07dfad1c49ce36f4223026a56f
frankfu03 <frankfu03@hotmail.com>
2022-11-05 21:43:28 +1100

    Add a timer component

--- src/app/typing-dashboard/typing-dashboard.component.ts
+++ src/app/typing-dashboard/typing-dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter,
-         HostListener, Input, OnInit } from '@angular/core';
+         HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { Key } from './../shared/key';
 import { IgnoreKey} from './../shared/ignore-key';
+import { TimerComponent } from '../timer/timer.component';
 
 
 @Component({
@@ -17,19 +18,17 @@ export class TypingDashboardComponent implements OnInit {
   @Input()
   public isRunning: boolean = false;
 
+  @ViewChild(TimerComponent)
+  private timerComponent!: TimerComponent;
+
   public typedText: string;
   public wpm: number;
   public numberOfErrorChars: number;
   public index: number;
 
-  private startTime: number;
-  private elapsedTime: number;
-
   constructor() {
     this.wpm = 0;
-    this.elapsedTime = 0;
     this.numberOfErrorChars = 0;
-    this.startTime = 0;
     this.index = 0;
     this.typedText = '';
   }
@@ -41,7 +40,7 @@ export class TypingDashboardComponent implements OnInit {
     if (!this.isRunning || this.index >= this.inputText.length) {
       return;
     } else if (event.key === Key.Escape) {
-      this.stopTiming();
+      this.stopTyping();
     } else if (event.key === Key.Backspace) {
       this.index --;
       this.typedText = this.typedText.substring(0, this.index);
@@ -50,14 +49,14 @@ export class TypingDashboardComponent implements OnInit {
     } else {
       // Start timing from the typing of the first character
       if (this.index === 0) {
-        this.startTiming();
+        this.startTyping();
       }
 
       const keyTyped = event.key;
       if (keyTyped !== this.inputText.charAt(this.index)) {
         this.numberOfErrorChars ++;
       } else if (keyTyped === Key.Enter) {
-        this.stopTiming();
+        this.stopTyping();
       } else {
         this.typedText += keyTyped;
         this.index ++;
@@ -65,49 +64,37 @@ export class TypingDashboardComponent implements OnInit {
 
       // Automatically stop timing if reaching the end of sample text
       if (this.index === this.inputText.length) {
-        this.stopTiming();
+        this.stopTyping();
       }
     }
   }
 
   ngOnInit(): void {
-    this.elapsedTime = 0;
   }
 
-  public startTiming(): void {
-    this.elapsedTime = 0;
-    this.startTime = new Date().getTime();
+  public startTyping(): void {
+    this.timerComponent.start();
     this.wpm = 0;
     this.index = 0;
     this.numberOfErrorChars = 0;
     this.typedText = '';
   }
 
-  public stopTiming(): void {
-    const stopTime = new Date().getTime();
-    this.elapsedTime += stopTime - this.startTime;
-    this.computeTypingSpeed();
-  }
-
-  public pauseTiming(): void {
-    const stopTime = new Date().getTime();
-    this.elapsedTime += stopTime - this.startTime;
-    this.startTime = 0;
-  }
-
-  public resumeTiming(): void {
-    this.startTime = new Date().getTime();
+  public stopTyping(): void {
+    this.timerComponent.stop();
+    const elapsedTime = this.timerComponent.getTimeElapsed();
+    this.computeTypingSpeed(elapsedTime);
   }
 
   /**
-   * Returns the number of words per minutes as typing speed.
+   * Computes the number of words per minutes as typing speed.
    * 
    * Assume that a word has a average of 5 characters.
    */
-  private computeTypingSpeed(): void {
+  private computeTypingSpeed(elapsedTime: number): void {
     const CHARS_PER_WORD = 5;
     const words = this.typedText.length / CHARS_PER_WORD;
-    const minutes = this.elapsedTime / (1000 * 60);
+    const minutes = elapsedTime / (1000 * 60);
     this.wpm = Math.round(words / minutes);
   }
 }
